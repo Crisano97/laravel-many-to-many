32ee10e38e5b2a9b02ff9904aa1a1d56bcbfff93
Crisano97 <97keb6@gmail.com>
2022-09-22 15:08:31 +0200

    creating posts_tags table and seeder

---
+++ database/seeds/PostsTagsTableSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+use App\Models\Post;
+use App\Models\Tag;
+use Illuminate\Database\Seeder;
+
+class PostsTagsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $posts = Post::all();
+        $tags = Tag::all();
+    }
+}
